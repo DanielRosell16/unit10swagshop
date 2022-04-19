cb2990c2263e25652e8ef5ce7017e5b8ce493c79
Dan <you@example.com>
2022-04-19 13:57:41 -0600

    I created a swag shop that business can order swag off of.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const hb = document.querySelector('#hamburgerBtn');
+
+const pn = document.querySelector('#primaryNav');
+
+hb.addEventListener('click', () => {
+     hb.classList.toggle('open')
+     pn.classList.toggle('open')
+
+})
